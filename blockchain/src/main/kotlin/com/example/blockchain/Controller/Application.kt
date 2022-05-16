836c699f14125e31adef846086e5f326148e4fb8
Jonathan Street <jonathan@jonathanstreet.com>
2022-05-16 21:07:21 +0100

    Revoke credential and get transaction ID

--- blockchain/src/main/kotlin/com/example/blockchain/Controller/Application.kt
+++ blockchain/src/main/kotlin/com/example/blockchain/Controller/Application.kt
@@ -19,6 +19,7 @@ import io.iohk.atala.prism.api.*
 import io.iohk.atala.prism.api.models.AtalaOperationId
 import io.iohk.atala.prism.api.models.AtalaOperationStatus
 import io.iohk.atala.prism.api.node.*
+import io.iohk.atala.prism.common.PrismSdkInternal
 import io.iohk.atala.prism.credentials.json.JsonBasedCredential
 import io.iohk.atala.prism.crypto.MerkleInclusionProof
 import io.iohk.atala.prism.crypto.Sha256Digest
@@ -35,6 +36,7 @@ import org.springframework.web.bind.annotation.*
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import pbandk.ByteArr
 
 data class NewDIDPost(
     var mnemonic: List<String> = listOf<String>(),
@@ -48,7 +50,8 @@ data class NewDIDPost(
 
 data class CheckDIDPost(
     val creation_operation_id: String,
-    var status: String = ""
+    var status: String = "",
+    var transaction_id: String = ""
 )
 
 data class NewCredentialPost(
@@ -59,18 +62,37 @@ data class NewCredentialPost(
     var creation_operation_id: String = "",
     var signed_credential_content: String = "",
     var signed_credential_canonical: String = "",
-    var signed_credential_proof: Any = ""
+    var signed_credential_proof: Any = "",
+    var signed_credential_hash: String = "",
+    var operation_hash: String = "",
+    var batch_id: String = ""
 )
 
 data class CheckCredentialPost(
     val creation_operation_id: String,
-    var status: String = ""
+    var status: String = "",
+    var transaction_id: String = ""
 )
 
 data class VerifyCredentialPost(
     val signed_credential: String = "",
     val signed_credential_proof: String = "",
     var errors: List<Any> = listOf<String>()
+)
+
+data class RevokeCredentialPost(
+    val issuer_mnemonic: List<String>,
+    val issuer_passphrase: String,
+    val operation_hash: String,
+    val batch_id: String,
+    val signed_credential_hash: String,
+    var revocation_operation_id: String = ""
+)
+
+data class CheckCredentialRevocationPost(
+    val revocation_operation_id: String,
+    var status: String = "",
+    var transaction_id: String = ""
 )
 
 data class DIDHolder(
@@ -91,6 +113,15 @@ fun generate_did(mnemonic: List<String>, passphrase: String): DIDHolder {
         revocationKeyPair.publicKey
     )
     return DIDHolder(masterKeyPair, issuingKeyPair, revocationKeyPair, unpublishedDID)
+}
+
+@PrismSdkInternal
+fun transactionId(oid: AtalaOperationId): String {
+    val node = NodeServiceCoroutine.Client(GrpcClient(grpcOptions))
+    val response = runBlocking {
+        node.GetOperationInfo(GetOperationInfoRequest(ByteArr(oid.value())))
+    }
+    return response.transactionId
 }
 
 val environment = "ppp.atalaprism.io"
@@ -135,6 +166,7 @@ class Application {
         return new_did
     }
 
+    @PrismSdkInternal
     @PostMapping("/DID_status/")
     fun check_did_status(@RequestBody check_did: CheckDIDPost): CheckDIDPost {
         // Check the status of a DID
@@ -144,6 +176,13 @@ class Application {
             nodeAuthApi.getOperationStatus(operation_id)
         }
         check_did.status = AtalaOperationStatus.asString(status)
+        if (
+            (status == AtalaOperationStatus.CONFIRMED_AND_APPLIED) ||
+            (status == AtalaOperationStatus.CONFIRMED_AND_REJECTED)
+        ) {
+            val transaction_id = transactionId(operation_id)
+            check_did.transaction_id = transaction_id
+        }
         return check_did
     }
 
@@ -189,10 +228,14 @@ class Application {
         new_credential.signed_credential_content = holderSignedCredential.content.fields.toString()
         new_credential.signed_credential_canonical = holderSignedCredential.canonicalForm
         new_credential.signed_credential_proof = holderCredentialMerkleProof.encode()
+        new_credential.signed_credential_hash = holderSignedCredential.hash().hexValue
+        new_credential.operation_hash = issueCredentialsInfo.operationHash.hexValue
+        new_credential.batch_id = issueCredentialsInfo.batchId.id
         // holderSignedCredential.content::class.java.methods.forEach(::println)
         return new_credential
     }
 
+    @PrismSdkInternal
     @PostMapping("/credential_status/")
     fun check_credential_status(@RequestBody check_credential: CheckCredentialPost): CheckCredentialPost {
         // Check the status of a credential
@@ -202,6 +245,13 @@ class Application {
             nodeAuthApi.getOperationStatus(operation_id)
         }
         check_credential.status = AtalaOperationStatus.asString(status)
+        if (
+            (status == AtalaOperationStatus.CONFIRMED_AND_APPLIED) ||
+            (status == AtalaOperationStatus.CONFIRMED_AND_REJECTED)
+        ) {
+            val transaction_id = transactionId(operation_id)
+            check_credential.transaction_id = transaction_id
+        }
         return check_credential
     }
 
@@ -220,5 +270,59 @@ class Application {
         }
         verify_cred.errors = status.verificationErrors
         return verify_cred
+    }
+
+    @PostMapping("/revoke_credential/")
+    fun revoke_credential(@RequestBody revoke_cred: RevokeCredentialPost): RevokeCredentialPost {
+        // Revoke credential
+        val issuer_did = generate_did(revoke_cred.issuer_mnemonic, revoke_cred.issuer_passphrase)
+        var issuerNodePayloadGenerator = NodePayloadGenerator(
+            issuer_did.did,
+            mapOf(
+                PrismDid.DEFAULT_MASTER_KEY_ID to issuer_did.masterKeyPair.privateKey,
+                PrismDid.DEFAULT_ISSUING_KEY_ID to issuer_did.issuingKeyPair.privateKey,
+                PrismDid.DEFAULT_REVOCATION_KEY_ID to issuer_did.revocationKeyPair.privateKey
+            )
+        )
+        val operation_hash = Sha256Digest.fromHex(revoke_cred.operation_hash)
+        val batch_id = Sha256Digest.fromHex(revoke_cred.batch_id)
+        val signed_credential_hash = Sha256Digest.fromHex(revoke_cred.signed_credential_hash)
+        val revokeCredentialsInfo = issuerNodePayloadGenerator.revokeCredentials(
+            PrismDid.DEFAULT_REVOCATION_KEY_ID,
+            operation_hash,
+            revoke_cred.batch_id,
+            arrayOf(signed_credential_hash)
+        )
+        val revokeCredentialsOperationId = runBlocking {
+            nodeAuthApi.revokeCredentials(
+                revokeCredentialsInfo.payload,
+                issuer_did.did.asCanonical(),
+                PrismDid.DEFAULT_REVOCATION_KEY_ID,
+                operation_hash,
+                revoke_cred.batch_id
+            )
+        }
+        revoke_cred.revocation_operation_id = revokeCredentialsOperationId.hexValue()
+        return revoke_cred
+    }
+
+    @PrismSdkInternal
+    @PostMapping("/credential_revocation_status/")
+    fun check_credential_revocation_status(@RequestBody check_credential: CheckCredentialRevocationPost): CheckCredentialRevocationPost {
+        // Check the status of a credential
+        val operation_digest = Sha256Digest.fromHex(check_credential.revocation_operation_id)
+        val operation_id = AtalaOperationId(operation_digest)
+        val status = runBlocking {
+            nodeAuthApi.getOperationStatus(operation_id)
+        }
+        check_credential.status = AtalaOperationStatus.asString(status)
+        if (
+            (status == AtalaOperationStatus.CONFIRMED_AND_APPLIED) ||
+            (status == AtalaOperationStatus.CONFIRMED_AND_REJECTED)
+        ) {
+            val transaction_id = transactionId(operation_id)
+            check_credential.transaction_id = transaction_id
+        }
+        return check_credential
     }
 }
